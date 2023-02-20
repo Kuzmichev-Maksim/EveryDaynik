29178ddd003cdb0a9dbb1c74027746d4556bd860
Kuzmichev-Maksim <Kuzmichev-Maksim>
2023-02-21 01:50:45 +0300

    Кекс

---
+++ EveryDaynik/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using EveryDaynik;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace EveryDaynik
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static Datenotes notes;
+        public static DateTime selectedDate = DateTime.Today;
+        public MainWindow()
+        {
+            InitializeComponent();
+            notes = new Datenotes(selectedDate);
+            UpdateListDel();
+            Calendare.SelectedDate = selectedDate;
+        }
+
+        private void Create_Click(object sender, RoutedEventArgs e)
+        {
+            string title = NameNote.Text;
+            string description = DescriptionNote.Text;
+            notes.CreateNotes(selectedDate, title, description);
+            UpdateListDel();
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            string title = NameNote.Text;
+            string description = DescriptionNote.Text;
+            notes.EditNotes(selectedDate, title, description);
+            UpdateListDel();
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            notes.DeleteNotes(selectedId: notes.SelectedId);
+            UpdateListDel();
+        }
+
+        private void Calendare_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            notes.SelectedDate = Convert.ToDateTime(Calendare.Text);
+            UpdateListDel();
+
+        }
+        public void UpdateListDel()
+        {
+            selectedDate = notes.SelectedDate;
+            notes.ReadNotes();
+            ListNotes.Items.Clear();
+            foreach (Notes note in notes.TodayNotes)
+            {
+                if (note.date == selectedDate)
+                {
+                    ListNotes.Items.Add(note.name);
+                    notes.Window(selectedDate);
+                }
+            }
+            NameNote.Text = "";
+            DescriptionNote.Text = "";
+        }
+
+        private void ListNotes_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ListNotes.SelectedIndex != -1)
+            {
+                selectedDate = notes.SelectedDate;
+                notes.SelectedId = ListNotes.SelectedIndex;
+                Notes note = notes.TodayNotes[notes.SelectedId];
+                NameNote.Text = note.name;
+                DescriptionNote.Text = note.description;
+            }
+        }
+    }
+}
---
+++ EveryDaynik/Notes.cs
@@ -0,0 +1,126 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace EveryDaynik
+{
+    public class Notes
+    {
+        public int id;
+        public string name;
+        public string description;
+        public DateTime date = DateTime.Today;
+        public Notes(int id, string name, string description, DateTime date)
+        {
+            this.id = id;
+            this.name = name;
+            this.description = description;
+            this.date = date;
+        }
+    }
+    public class Datenotes
+    {
+        public List<Notes> TodayNotes;
+        public List<Notes> AllNotes;
+        public DateTime SelectedDate;
+        public int SelectedId = -1;
+
+        public Datenotes(DateTime date)
+        {
+            this.TodayNotes = DS.Deserialize<Notes>(date);
+            this.AllNotes = DS.Deserialize<Notes>(default);
+            SelectedDate = date;
+        }
+        public void CreateNotes(DateTime date, string title, string description)
+        {
+            Notes note = new Notes(this.AllNotes.Count, title, description, this.SelectedDate);
+            this.AllNotes.Add(note);
+            DS.Serialize<Notes>(this.AllNotes);
+            this.ReadNotes();
+        }
+        public void DeleteNotes(int selectedId = -1, int id = -1)
+        {
+            if (selectedId != -1)
+            {
+                id = this.TodayNotes[selectedId].id;
+            }
+            List<Notes> notes = new List<Notes>();
+            foreach (Notes note in this.AllNotes)
+            {
+                if (note.id != id)
+                {
+                    notes.Add(note);
+                }
+            }
+            this.AllNotes = notes;
+            ReadNotes();
+            ReloadNotes();
+        }
+        public void ReadNotes()
+        {
+            this.TodayNotes = DS.Deserialize<Notes>(this.SelectedDate);
+        }
+        public void ReloadNotes()
+        {
+            DS.Serialize<Notes>(this.AllNotes);
+        }
+        public void EditNotes(DateTime date, string title, string description)
+        {
+            if (SelectedId != -1)
+            {
+                Notes note = new Notes(this.TodayNotes[SelectedId].id, title, description, this.SelectedDate);
+                DeleteNotes(this.TodayNotes[SelectedId].id);
+                this.AllNotes.Add(note);
+                ReloadNotes();
+                this.ReloadNotes();
+                this.SelectedId = -1;
+            }
+        }
+        public void Window(DateTime date = default)
+        {
+            List<Notes> notes = new List<Notes>();
+            foreach (Notes note in this.AllNotes)
+            {
+                if (note.date == date) notes.Add(note);
+            }
+        }
+    }
+    public class DS
+    {
+            public static List<T> Serialize<T>(List<T> notes)
+            {
+                string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                string json = JsonConvert.SerializeObject(notes);
+                File.WriteAllText(desktop + "\\Zametki.json", json);
+                return notes;
+            }
+            public static List<T> Deserialize<T>(DateTime date = default)
+            {
+                try
+                {
+                    string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                    List<T> notes = new();
+                    List<T> json = JsonConvert.DeserializeObject<List<T>>(File.ReadAllText(desktop + "\\Zametki.json"));
+                    foreach (T note in json)
+                    {
+                        notes.Add(note);
+                    }
+                    return notes;
+                }
+                catch
+                {
+                    List<T> notes = new();
+                    File.WriteAllText("Zametki.json", JsonConvert.SerializeObject(notes));
+                    return notes;
+                }
+            }
+        
+    }
+}
